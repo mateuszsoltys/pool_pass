e70d4dc4862cb44fbc0855b0962d42158edd15de
Mateusz_Soltys <matssol.dev@gmail.com>
2022-11-04 22:13:48 +0000

    create new flutlab project

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:poolpass/app/features/pages/home_page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Pool Pass',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: HomePage(),
+    );
+  }
+}
+
