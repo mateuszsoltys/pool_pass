51c2ac1f7772262540e991f6e84d7e6e4cd568cb
Mateusz Soltys <admiral.soltys@google.com>
2022-11-09 12:11:14 +0100

    create indicator data model

---
+++ lib/app/models/data/usage_indicator_data_model.dart
@@ -0,0 +1,6 @@
+class UsageIndicatorDataModel {
+  final String? passName = null;
+  final DateTime passDate = DateTime.now();
+  final int? passRemainingTime = null;
+  final int? tickets = null;
+}
